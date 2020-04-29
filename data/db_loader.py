fb47e6f43e3383eb87dd7f78e29e90ee8413db15
RoozbehBandpey <r.bandpey67@gmail.com>
2020-04-29 23:56:50 +0200

    idk

--- data/db_loader.py
+++ data/db_loader.py
@@ -7,4 +7,7 @@ DATA_DIR = os.path.join(os.getcwd(), r'data')
 if os.path.exists(os.path.join(DATA_DIR, 'files_index.csv')):
 	df = pd.read_csv(os.path.join(DATA_DIR, 'files_index.csv'))
 
-print(df)
+# for index, row in df.iterrows():
+# 	print(row)
+
+print(df[:1]['file_names'])

f33e7c51d4a42fe4110d939f469c30286537be33
RoozbehBandpey <r.bandpey67@gmail.com>
2020-04-29 19:32:04 +0200

    file parser works

--- utils/dir_parser.py
+++ utils/dir_parser.py
@@ -1,30 +1,20 @@
 import os
 import json
-
-dirs = ['biorxiv_medrxiv']
+import pandas as pd
 
 DATA_DIR = os.path.join(os.getcwd(), r'data\CORD-19-research-challenge')
 
-sums = 0
+file_names = []
+relative_paths = []
 for dirpath, dnames, fnames in os.walk(DATA_DIR):
 	if len(fnames) > 0:
 		for file_name in fnames:
 			if file_name.endswith('.json'):
-				category = dirpath.strip(DATA_DIR)
-				print(file_name, category)
+				relative_path = dirpath.split(DATA_DIR)[-1]
+				file_names.append(file_name)
+				relative_paths.append(relative_path)
 
-# 	print(len(fnames))
-# 	if len(fnames) > 1000:
-# 		sums += len(fnames)
 
-# print(sums)
-#     for f in fnames:
-#         if f.endswith(".x"):
-#             x(os.path.join(dirpath, f))
-#         elif f.endswith(".xc"):
-#             xc(os.path.join(dirpath, f))
-
-# 	for file in os.listdir(os.path.join(os.path.dirname(
-#                 os.path.realpath(__file__)), f'CORD-19-research-challenge/{d}/{d}/pdf_json')):
-# 			print(file)
-			
+file_index = {'file_names': file_names, 'relative_paths': relative_paths}
+df = pd.DataFrame(file_index)
+df.to_csv(os.path.join(os.path.dirname(DATA_DIR), 'files_index.csv'))

fe5c272a98c560d054a93bd02d5f28d45311d469
RoozbehBandpey <r.bandpey67@gmail.com>
2020-05-07 22:06:42 +0200

    added blob handler

---
+++ utils/blob_handler.py
@@ -0,0 +1,10 @@
+import os
+import uuid
+from azure.storage.blob import BlobServiceClient
+
+try:
+    print("Azure Blob storage v12 - Python quickstart sample")
+    # Quick start code goes here
+except Exception as ex:
+    print('Exception:')
+    print(ex)

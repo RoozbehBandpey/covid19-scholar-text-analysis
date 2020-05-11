4fd12ef21aebc94c9e07df4e44a12d00d9987c0b
RoozbehBandpey <r.bandpey67@gmail.com>
2020-05-11 22:01:18 +0200

    removed

--- tests/test_blbtrigger_init.py
+++ tests/test_blbtrigger_init.py
@@ -5,13 +5,17 @@ import json
 import sys
 import unittest
 import azure.functions as func
+import ..traininglake_af_blbtrigger
 
-def test_input_stream():
-	test_file_name = 'test.json'
-	test_data = json.load(open(os.path.join(os.path.dirname(os.path.realpath(__file__)), test_file_name), 'rb'))
-	bytes_stream = func.InputStream
-	bytes_stream.name = test_file_name
-	bytes_stream.length = sys.getsizeof((test_data)
+
+class TestFunction(unittest.TestCase):
+	def test_input_stream(self):
+		test_file_name = 'test.json'
+		test_data = json.load(open(os.path.join(os.path.dirname(os.path.realpath(__file__)), test_file_name), 'rb'))
+		bytes_stream = func.InputStream
+		bytes_stream.name = test_file_name
+		bytes_stream.length = sys.getsizeof((test_data)
+                                      traininglake_af_blbtrigger.
 
 test_input_stream()
 
@@ -23,14 +27,14 @@ test_input_stream()
 # buffered_message = io.BytesIO(b"{'name': 'test', 'length': 10}")
 # print(buffered_message.getvalue())
 # test_data_bin = io.BytesIO(json.dumps(test_data).encode())
-test_data_bin = io.BufferedIOBase(json.dumps(test_data).encode())
-print(test_data)
-print(test_data_bin)
-print(type(test_data_bin))
+# test_data_bin = io.BufferedIOBase(json.dumps(test_data).encode())
+# print(test_data)
+# print(test_data_bin)
+# print(type(test_data_bin))
 
-bytes_stream = func.InputStream
-bytes_stream.write(test_data_bin)
-print(bytes_stream)
+# bytes_stream = func.InputStream
+# bytes_stream.write(test_data_bin)
+# print(bytes_stream)
 # test_data_bin.write(bytes_stream.read()
 # # bytes_stream = bytes_stream
 # bytes_stream.name = test_file_name

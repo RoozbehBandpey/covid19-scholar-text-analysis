21fe8644370c33221568420c221991a068cd04c4
RoozbehBandpey <r.bandpey67@gmail.com>
2020-05-11 18:28:37 +0200

    :zap:

--- tests/test_blbtrigger_init.py
+++ tests/test_blbtrigger_init.py
@@ -12,13 +12,19 @@ import azure.functions as func
 # buffered_message = io.BufferedIOBase(b"Test")
 # buffered_message = io.BytesIO(b"{'name': 'test', 'length': 10}")
 # print(buffered_message.getvalue())
-stream = func.InputStream.name('test')
-print(stream)
+message = 'test'
+bytes_stream = func.InputStream
+bytes_stream.name = message
+bytes_stream.length = len(message.encode('utf-8'))
+# bytes_stream = func.InputStream.length = len(message.encode('utf-8'))
+# stream = func.InputStream.read(bytes_stream)
+print(bytes_stream)
 
 
-# def functionsss(incommingblob: func.InputStream):
-#     print(f"Python blob trigger function processed blob \n"
-#                  f"Name: {incommingblob.name}\n"
-#                  f"Blob Size: {incommingblob.length} bytes")
+def functionsss(incommingblob: func.InputStream):
+    print(f"Python blob trigger function processed blob \n"
+                 f"Name: {incommingblob.name}\n"
+                 f"Blob Size: {incommingblob.length} bytes")
 
-# functionsss(buffered_message)
+
+functionsss(bytes_stream)

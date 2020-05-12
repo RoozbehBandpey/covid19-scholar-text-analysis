e5f49712f9595fb20fd56ce24b07bfedb755fc8c
RoozbehBandpey <r.bandpey67@gmail.com>
2020-05-12 14:29:57 +0200

    restructured for deployment

--- data/extractor.py
+++ data/extractor.py
@@ -1,5 +1,5 @@
 import os
-
+import pandas as pd
 
 
 
@@ -12,18 +12,18 @@ class Extractor():
 	def _dir_parser(self):
 		file_names = []
 		relative_paths = []
-		for dirpath, dnames, fnames in os.walk(DATA_DIR):
+		for dirpath, dnames, fnames in os.walk(self.DATASET_DIR):
 			if len(fnames) > 0:
 				for file_name in fnames:
 					if file_name.endswith('.json'):
-						relative_path = dirpath.split(DATA_DIR)[-1]
+						relative_path = dirpath.split(self.DATASET_DIR)[-1]
 						file_names.append(file_name)
 						relative_paths.append(relative_path)
 
 
 		file_index = {'file_names': file_names, 'relative_paths': relative_paths}
 		df = pd.DataFrame(file_index)
-		df.to_csv(os.path.join(os.path.dirname(DATA_DIR), 'files_index.csv'))
+		df.to_csv(os.path.join(os.path.dirname(self.DATASET_DIR), 'files_index.csv'))
 
 	def __normalize_space(self, text):
 		return " ".join(text.split())

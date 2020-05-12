839935198754bcc6eb6c2dcd61f7d8a9c433509b
RoozbehBandpey <r.bandpey67@gmail.com>
2020-05-12 13:11:08 +0200

    added dir parser

--- data/extractor.py
+++ data/extractor.py
@@ -9,6 +9,21 @@ class Extractor():
 		self.DATA_DIR = os.path.join(os.getcwd(), 'data')
 		self.DATASET_DIR = os.path.join(self.DATA_DIR, 'CORD-19-research-challenge')
 
+	def _dir_parser(self):
+		file_names = []
+		relative_paths = []
+		for dirpath, dnames, fnames in os.walk(DATA_DIR):
+			if len(fnames) > 0:
+				for file_name in fnames:
+					if file_name.endswith('.json'):
+						relative_path = dirpath.split(DATA_DIR)[-1]
+						file_names.append(file_name)
+						relative_paths.append(relative_path)
+
+
+		file_index = {'file_names': file_names, 'relative_paths': relative_paths}
+		df = pd.DataFrame(file_index)
+		df.to_csv(os.path.join(os.path.dirname(DATA_DIR), 'files_index.csv'))
 
 	def __normalize_space(self, text):
 		return " ".join(text.split())

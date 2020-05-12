16b5f0844d6c4aff41d12ce65cda23f333f22942
RoozbehBandpey <r.bandpey67@gmail.com>
2020-05-12 12:05:58 +0200

    change returning value

--- traininglake_af_blbtrigger/__init__.py
+++ traininglake_af_blbtrigger/__init__.py
@@ -3,9 +3,8 @@ import logging
 import azure.functions as func
 
 
-def main(incommingblob: func.InputStream) -> int:
+def main(incommingblob: func.InputStream):
 	logging.info(f"Python blob trigger function processed blob \n"
 				 f"Name: {incommingblob.name}\n"
 				 f"Blob Size: {incommingblob.length} bytes")
 
-	return incommingblob.length

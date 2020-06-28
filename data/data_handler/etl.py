8aaaac26097ed015593653f7f8a7982507c40e64
RoozbehBandpey <r.bandpey67@gmail.com>
2020-06-28 11:51:40 +0200

    got creds

--- data/data_handler/etl.py
+++ data/data_handler/etl.py
@@ -1,12 +1,21 @@
-from loader import Loader
-from extractor import Extractor
+# from loader import Loader
+# from extractor import Extractor
+from kaggle.api.kaggle_api_extended import KaggleApi
 
 
 
 class ETL():
 	def __init__(self):
-		self.loader_obj = Loader()
-		self.extractor_obj = Extractor()
+		# self.loader_obj = Loader()
+		# self.extractor_obj = Extractor()
+		pass
 
 
-	def download(self)
+	def download(self):
+		api = KaggleApi()
+		api.authenticate()
+
+
+if __name__ == "__main__":
+	etl = ETL()
+	etl.download()
